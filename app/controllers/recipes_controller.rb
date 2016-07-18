b2b184e986e2dc294254d1855fe8a7e41f9b087d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-18 11:25:32 +0100

    add actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,33 @@
 class RecipesController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def public_list
+
+  end
+
+  def my_list
+
+  end
 end
