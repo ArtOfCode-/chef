fd65738104e094a4a15568f96df803fd2ca50ab8
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-18 16:13:29 +0100

    custom error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,2 +1,21 @@
 class ErrorsController < ApplicationController
+  def forbidden
+    render :status => 403
+  end
+
+  def not_found
+    render :status => 404
+  end
+
+  def conflict
+    render :status => 409
+  end
+
+  def unprocessable
+    render :status => 422
+  end
+
+  def internal
+    render :status => 500
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,10 @@ Rails.application.routes.draw do
   delete 'recipes/:id',                        :to => 'recipes#destroy'
   get    'recipes/:id/edit',                   :to => 'recipes#edit'
   patch  'recipes/:id/edit',                   :to => 'recipes#update'
+
+  match  '/403',                               :to => 'errors#forbidden',       :via => :all
+  match  '/404',                               :to => 'errors#not_found',       :via => :all
+  match  '/409',                               :to => 'errors#conflict',        :via => :all
+  match  '/422',                               :to => 'errors#unprocessable',   :via => :all
+  match  '/500',                               :to => 'errors#internal',        :via => :all
 end
