7dc83ffb7615363e6c3adf8de18fe748177ae2e0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-18 19:36:51 +0100

    try a different method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,15 +3,18 @@ class Recipe < ApplicationRecord
   belongs_to :access_level
 
   def self.publics
-    Recipe.joins(:access_levels).where(:access_level => { :name => 'Public' })
+    #Recipe.joins(:access_levels).where(:access_level => { :name => 'Public' })
+    AccessLevel.where(:name => 'Public').first.recipes
   end
 
   def self.internals
-    Recipe.joins(:access_levels).where(:access_level => { :name => 'Internal' })
+    #Recipe.joins(:access_levels).where(:access_level => { :name => 'Internal' })
+    AccessLevel.where(:name => 'Internal').first.recipes
   end
 
   def self.users(user)
-    Recipe.joins(:access_levels).where(:access_level => { :name => 'Private' }, :user => user)
+    #Recipe.joins(:access_levels).where(:access_level => { :name => 'Private' }, :user => user)
+    AccessLevel.where(:name => 'Private').first.recipes.where(:user => user)
   end
 
   def has_access(user)
