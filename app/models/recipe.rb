c41f086cfe47f6416a28f2ede80124f829de37aa
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-18 18:51:35 +0100

    recipe group helpers

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,18 @@ class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :access_level
 
+  def self.public
+    Recipe.joins(:access_levels).where(:access_level => { :name => 'Public' })
+  end
+
+  def self.internal
+    Recipe.joins(:access_levels).where(:access_level => { :name => 'Internal' })
+  end
+
+  def self.user(user)
+    Recipe.joins(:access_levels).where(:access_level => { :name => 'Private' }, :user => user)
+  end
+
   def has_access(user)
     if self.access_level.name == 'Internal'
       return true
