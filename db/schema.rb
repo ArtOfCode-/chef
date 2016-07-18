9416570a300a0a1c8c9a2647fc7380654e8f94d1
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-18 11:01:07 +0000

    add is_admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718091527) do
+ActiveRecord::Schema.define(version: 20160718102744) do
+
+  create_table "access_levels", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "method"
+    t.integer  "time"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "access_level_id"
+    t.index ["access_level_id"], name: "index_recipes_on_access_level_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
