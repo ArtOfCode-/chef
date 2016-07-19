2f05df5ef62662925a5d9cecf6a0f7c17ecaed15
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-19 20:21:52 +0000

    complete database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718134511) do
+ActiveRecord::Schema.define(version: 20160719185310) do
 
   create_table "access_levels", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
