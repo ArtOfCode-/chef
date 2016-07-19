29f929681affc9269522c3b60867bcfc17fca02a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-19 21:01:35 +0100

    favorite control methods

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_recipe
+
+  def toggle_favorite
+    existing = Favorite.where(:user => current_user, :recipe => @recipe)
+    unless existing.exists?
+      @favorite = Favorite.new(:user => current_user, :recipe => @recipe)
+      if @favorite.save
+        render :json => { :status => 'success', :remove_class => 'glyphicon-heart-empty', :add_class => 'glyphicon-heart' } and return
+      else
+        render :json => { :status => 'failed', :message => 'Object failed to save.' }, :status => 500 and return
+      end
+    else
+      if existing.destroy
+        render :json => { :status => 'success', :remove_class => 'glyphicon-heart', :add_class => 'glyphicon-heart-empty' } and return
+      else
+        render :json => { :status => 'failed', :message => 'Failed to destroy object.' }, :status => 500 and return
+      end
+    end
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find params[:id]
+    end
 end
