8531955244ff6fd3450ba9ede4e50439bed9037d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-20 14:48:16 +0100

    commented recipes to front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
   def index
     @featured = Rails.cache.fetch('featured_recipes', :expires_in => 1.day) do
-      Recipe.publics.joins(:favorites).where(:created_at => 1.day.ago..Time.now).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
+      Recipe.publics.joins(:favorites).where(:updated_at => 1.day.ago..Time.now).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
     end
     @favorited = Rails.cache.fetch('favorited_recipes', :expires_in => 30.minutes) do
       Recipe.publics.joins(:favorites).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
+    end
+    @commented = Rails.cache.fetch('commented_recipes', :expires_in => 30.minutes) do
+      Recipe.publics.joins(:comments).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
     end
   end
 end
