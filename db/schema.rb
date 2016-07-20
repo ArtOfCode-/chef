90b1cf5a88e1efffb0f8eac92b6b3963712c6823
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-20 09:58:07 +0000

    add comments controller & model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719185310) do
+ActiveRecord::Schema.define(version: 20160719202129) do
 
   create_table "access_levels", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20160719185310) do
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
