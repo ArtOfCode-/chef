78d4e7ba2743ebadebe553bb932474e3b6f6c50d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-20 11:05:12 +0100

    only use public recipes on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     @featured = Rails.cache.fetch('featured_recipes', :expires_in => 1.day) do
-      Recipe.joins(:favorites).where(:created_at => 1.day.ago..Time.now).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
+      Recipe.publics.joins(:favorites).where(:created_at => 1.day.ago..Time.now).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
     end
     @favorited = Rails.cache.fetch('favorited_recipes', :expires_in => 30.minutes) do
-      Recipe.joins(:favorites).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
+      Recipe.publics.joins(:favorites).group('recipes.id').order('COUNT(recipes.id) DESC').limit(3)
     end
   end
 end
