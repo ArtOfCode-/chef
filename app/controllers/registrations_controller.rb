86d3c8e5ddc1f749f008883aac746d59e86211b6
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-07-24 17:56:55 +0100

    get cache write syntax right

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     super
-    Rails.cache.write('users_newest') do
-      User.all.order(:created_at => :desc)
-    end
-    Rails.cache.write('users_recipes') do
-      User.all.joins(:recipes).group('users.id').order('COUNT(users.id) DESC')
-    end
-    Rails.cache.write('users_alpha') do
-      User.all.order(:username => :asc)
-    end
+    Rails.cache.write('users_newest', User.all.order(:created_at => :desc))
+    Rails.cache.write('users_recipes', User.all.joins(:recipes).group('users.id').order('COUNT(users.id) DESC'))
+    Rails.cache.write('users_alpha', User.all.order(:username => :asc))
   end
 end
